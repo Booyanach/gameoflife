ba26ad03f29e774891a10b311d6e01c6526eb931
Gonçalo Vieira <sandwave@gmail.com>
2017-03-04 15:20:21 +0000

    Main functionality added.
    * Added update functionality and a way to calculate the behavior of cells according to their immediate neighbors.
    
    * Using window.requestAnimationFrame, could use the cancel command for stopping but the updates are so fast that simply checking for a flag will suffice.

--- js/gol.js
+++ js/gol.js
@@ -13,24 +13,69 @@ GOL.prototype.generateGrid = function () {
     this.height = parseInt(document.getElementById('y_input').value);
 
     this.grid = new Array(this.width).fill(new Array(this.height).fill(0));
-
-    console.log(this.grid);
+    this.canvas.width = this.width * this.cellSize;
+    this.canvas.height = this.height * this.cellSize;
 }
 
-/**
- * We'll be using a canvas to draw this
- */
 GOL.prototype.drawGrid = function () {
     var self = this;
     self.clear();
-    self.grid = self.grid.map(function(row, irow) {
-        return row.map(function(cell, icell) {
+    self.grid.map(function(row, iRow) {
+        row.map(function(cell, iCell) {
             // Either occupied or not
             self.context.fillStyle = '#7fafd1';
-            if (cell === 1) self.context.fillRect(irow * self.cellSize, icell * self.cellSize, self.cellSize, self.cellSize);
+            if (cell === 1) self.context.fillRect(iRow * self.cellSize, iCell * self.cellSize, self.cellSize, self.cellSize);
         });
     });
 }
+
+GOL.prototype.updateGrid = function (callback) {
+    var self = this;
+    self.grid = self.grid.map(function(row, iRow) {
+        return row.map(function(cell, iCell) {
+            var neighbors = self.getNeighbors(row, iRow, iCell);
+            return self.calculateBehavior(neighbors, cell);
+        });
+    });
+}
+
+GOL.prototype.getNeighbors = function (row, iRow, iCell) {
+    var neighbors = 0;
+
+    var prevRow = this.grid[iRow-1];
+    var nextRow = this.grid[iRow+1];
+
+    if(prevRow) {
+        neighbors += prevRow[iCell-1] || 0;         //top left
+        neighbors += prevRow[iCell] || 0;           //top center
+        neighbors += prevRow[iCell+1] || 0;         //top right
+    }
+
+    neighbors += row[iCell-1] || 0;                 //middle left
+    neighbors += row[iCell+1] || 0;                 //middle right
+
+    if (nextRow) {
+        neighbors += nextRow[iCell-1] || 0;         //bottom left
+        neighbors += nextRow[iCell] || 0;           //bottom center
+        neighbors += nextRow[iCell+1] || 0;         //bottom right
+    }
+
+    return neighbors;
+}
+
+GOL.prototype.calculateBehavior = function (neighbors, cell) {
+    if (cell === 0) {
+        // Has 3 living neighbors, make it live, otherwise, remain dead
+        cell = neighbors === 3 ? 1 : cell;
+    } else {
+        if ([0, 1, 4, 5, 6, 7, 8].indexOf(neighbors) > -1) {
+            // Either the cell is lonely (0, 1) or the space is overcrowded (4, 5, 6, 7, 8)
+            // Otherwise, it keeps on living
+            cell = 0;
+        }
+    }
+    return cell;
+};
 
 GOL.prototype.populate = function () {
     this.randomizedStart();
@@ -38,7 +83,11 @@ GOL.prototype.populate = function () {
 }
 
 GOL.prototype.nextStep = function () {
-
+    this.drawGrid();
+    this.updateGrid();
+    if (this.started) {
+        window.requestAnimationFrame(this.nextStep.bind(this));
+    }
 }
 
 GOL.prototype.startStop = function (elem) {
@@ -48,14 +97,13 @@ GOL.prototype.startStop = function (elem) {
     } else {
         elem.innerText = "Start";
     }
-
-    // this.calculate();
+    this.nextStep();
 };
 
 GOL.prototype.randomizedStart = function () {
     this.grid = this.grid.map(function(row) {
         return row.map(function(cell) {
-            // Either occupied or not
+            // Either alive or not
             return Math.floor(Math.random() * 2);
         });
     });
